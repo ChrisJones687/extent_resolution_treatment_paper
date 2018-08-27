dcf29071aaaad8e7ae76cfe94e474c9a70e8da24
Chris Michael Jones <cjones1688@gmail.com>
2018-08-27 16:01:01 -0400

    updated to read in the finished model simulations and examining the data

--- analysis_and_figures.R
+++ analysis_and_figures.R
@@ -7,11 +7,18 @@ library(data.table)
 library(ggplot2)
 
 # Create Directory and list of files with all log files from extent resolution runs
-directories_full = list.dirs("C:/Users/Chris/Google Drive/landis_extent_resolution_runs/full", full.names = TRUE)
-directories_half = list.dirs("C:/Users/Chris/Google Drive/landis_extent_resolution_runs/half", full.names = TRUE)
-directories_quarter = list.dirs("C:/Users/Chris/Google Drive/landis_extent_resolution_runs/quarter", full.names = TRUE)
-directories_eighth = list.dirs("C:/Users/Chris/Google Drive/landis_extent_resolution_runs/eighth", full.names = TRUE)
-directories_sixteenth = list.dirs("C:/Users/Chris/Google Drive/landis_extent_resolution_runs/sixtenth", full.names = TRUE)
+directories_full = list.dirs("D:/landis_extent_resolution_runs/full", full.names = TRUE)
+directories_half = list.dirs("D:/landis_extent_resolution_runs/half", full.names = TRUE)
+directories_quarter = list.dirs("D:/landis_extent_resolution_runs/quarter", full.names = TRUE)
+directories_eighth = list.dirs("D:/landis_extent_resolution_runs/eighth", full.names = TRUE)
+directories_sixteenth = list.dirs("D:/landis_extent_resolution_runs/sixtenth", full.names = TRUE)
+
+size_full = ncell(raster("D:/landis_extent_resolution_runs/full/bigsur_eda_30m_1/ecoregions_30.tif"))*30*30
+size_half = ncell(raster("D:/landis_extent_resolution_runs/half/bigsur_eda_30m_1/ecoregions_30.tif"))*30*30
+size_quarter = ncell(raster("D:/landis_extent_resolution_runs/quarter/bigsur_eda_30m_1/ecoregions_30.tif"))*30*30
+size_eighth = ncell(raster("D:/landis_extent_resolution_runs/eighth/bigsur_eda_30m_1/ecoregions_30.tif"))*30*30
+size_sixteenth = ncell(raster("D:/landis_extent_resolution_runs/sixtenth/bigsur_eda_30m_1/ecoregions_30.tif"))*30*30
+
 
 files_full_eda = list.files(path= directories_full, pattern="eda-log.csv", full.names = TRUE)
 files_half_eda = list.files(path= directories_half, pattern="eda-log.csv", full.names = TRUE)
@@ -31,29 +38,90 @@ resolution_list <- c(rep(270,10), rep(30,10), rep(810,10), rep(90,10))
 ## calculate total diseased area (number of diseased sites isn't comparable across resolutions)
 for (i in 1:length(eda_sixteenth_list)) {
   eda_sixteenth_list[[i]]$diseased_area <- eda_sixteenth_list[[i]]$DiseasedSites*area_list[[i]]/(1*10^6)
+  eda_sixteenth_list[[i]]$percent_diseased_area <- eda_sixteenth_list[[i]]$DiseasedSites*area_list[[i]]/size_sixteenth
   eda_sixteenth_list[[i]]$resolution <- resolution_list[[i]]
   eda_sixteenth_list[[i]]$X <- 0
   eda_sixteenth_list[[i]]$AgentName <- 0
+  
+  eda_eighth_list[[i]]$diseased_area <- eda_eighth_list[[i]]$DiseasedSites*area_list[[i]]/(1*10^6)
+  eda_eighth_list[[i]]$percent_diseased_area <- eda_eighth_list[[i]]$DiseasedSites*area_list[[i]]/size_eighth
+  eda_eighth_list[[i]]$resolution <- resolution_list[[i]]
+  eda_eighth_list[[i]]$X <- 0
+  eda_eighth_list[[i]]$AgentName <- 0
+  
+  eda_quarter_list[[i]]$diseased_area <- eda_quarter_list[[i]]$DiseasedSites*area_list[[i]]/(1*10^6)
+  eda_quarter_list[[i]]$percent_diseased_area <- eda_quarter_list[[i]]$DiseasedSites*area_list[[i]]/size_quarter
+  eda_quarter_list[[i]]$resolution <- resolution_list[[i]]
+  eda_quarter_list[[i]]$X <- 0
+  eda_quarter_list[[i]]$AgentName <- 0
+  
+  eda_half_list[[i]]$diseased_area <- eda_half_list[[i]]$DiseasedSites*area_list[[i]]/(1*10^6)
+  eda_half_list[[i]]$percent_diseased_area <- eda_half_list[[i]]$DiseasedSites*area_list[[i]]/size_half
+  eda_half_list[[i]]$resolution <- resolution_list[[i]]
+  eda_half_list[[i]]$X <- 0
+  eda_half_list[[i]]$AgentName <- 0
+  
+  eda_full_list[[i]]$diseased_area <- eda_full_list[[i]]$DiseasedSites*area_list[[i]]/(1*10^6)
+  eda_full_list[[i]]$percent_diseased_area <- eda_full_list[[i]]$DiseasedSites*area_list[[i]]/size_full
+  eda_full_list[[i]]$resolution <- resolution_list[[i]]
+  eda_full_list[[i]]$X <- 0
+  eda_full_list[[i]]$AgentName <- 0
 }
 
-eda_mean <- as.data.frame(rbindlist(eda_sixteenth_list)[,lapply(.SD,mean), list(Time, resolution)])
-eda_sd <- as.data.frame(rbindlist(eda_sixteenth_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_sixteenth <- as.data.frame(rbindlist(eda_sixteenth_list)[,lapply(.SD,mean), list(Time, resolution)])
+eda_sd_sixteenth <- as.data.frame(rbindlist(eda_sixteenth_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_sixteenth$extent <- 0.0625
+
+eda_mean_eighth <- as.data.frame(rbindlist(eda_eighth_list)[,lapply(.SD,mean), list(Time, resolution)])
+eda_sd_eighth <- as.data.frame(rbindlist(eda_eighth_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_eighth$extent <- 0.125
+
+eda_mean_quarter <- as.data.frame(rbindlist(eda_quarter_list)[,lapply(.SD,mean), list(Time, resolution)])
+eda_sd_quarter <- as.data.frame(rbindlist(eda_quarter_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_quarter$extent <- 0.25
+
+eda_mean_half <- as.data.frame(rbindlist(eda_half_list)[,lapply(.SD,mean), list(Time, resolution)])
+eda_sd_half <- as.data.frame(rbindlist(eda_half_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_half$extent <- 0.5
+
+eda_mean_full <- as.data.frame(rbindlist(eda_full_list)[,lapply(.SD,mean), list(Time, resolution)])
+eda_sd_full <- as.data.frame(rbindlist(eda_full_list)[,lapply(.SD,sd), list(Time, resolution)])
+eda_mean_full$extent <- 1
+
+eda_mean <- rbind(eda_mean_sixteenth,eda_mean_eighth,eda_mean_quarter,eda_mean_half, eda_mean_full)
+eda_sd <- rbind(eda_sd_sixteenth,eda_sd_eighth,eda_sd_quarter,eda_sd_half, eda_sd_full)
+
+eda_mean$min_percent_diseased_area <- eda_mean$percent_diseased_area-(eda_sd$percent_diseased_area/7)
+eda_mean$max_percent_diseased_area <- eda_mean$percent_diseased_area+(eda_sd$percent_diseased_area/7)
+
 
 title = "Diseased area over time"
 theme = theme_set(theme_minimal())
 theme = theme_update(legend.position="top", legend.title=element_blank())
 theme = theme_update(axis.text = element_text(colour="black"), axis.ticks=element_blank(), plot.title = element_text(hjust = 0.5), axis.line = element_line())
-plot = ggplot(eda_mean, aes(Time, diseased_area, color=factor(resolution)))+geom_line(aes(color = factor(resolution)), size=1)
+plot = ggplot(eda_mean, aes(Time, percent_diseased_area, color=factor(resolution), shape = factor(extent)))+geom_line(aes(color = factor(resolution), linetype = factor(extent)), size=1)
+#plot = plot+scale_color_manual(values=c("#CC0000","#7BAFD4", "#CD660D"))+scale_fill_manual(values=c("#CC0000","#7BAFD4", "#CD660D"))
+plot = plot+ggtitle(title)
+plot = plot + theme(axis.text=element_text(size=10),axis.title=element_text(size=16,vjust=0.35),legend.text=element_text(size=12),plot.title=element_text(size=22))
+plot = plot + scale_x_continuous(name="Date", breaks=seq(2000,2100,20))
+plot = plot + scale_y_continuous(name=expression("Diseased area (m^2"))
+plot = plot +geom_ribbon(data = eda_mean, aes(ymin=min_percent_diseased_area, ymax=max_percent_diseased_area, fill = factor(resolution), color = factor(resolution)), alpha=0.3)
+#plot= plot+theme(legend.position="none")
+plot
+
+
+title = "Diseased area over time"
+theme = theme_set(theme_minimal())
+theme = theme_update(legend.position="top", legend.title=element_blank())
+theme = theme_update(axis.text = element_text(colour="black"), axis.ticks=element_blank(), plot.title = element_text(hjust = 0.5), axis.line = element_line())
+plot = ggplot(eda_mean_quarter, aes(Time, diseased_area, color=factor(resolution), shape = factor(extent)))+geom_line(aes(color = factor(resolution), linetype = factor(extent)), size=1)
 #plot = plot+scale_color_manual(values=c("#CC0000","#7BAFD4", "#CD660D"))+scale_fill_manual(values=c("#CC0000","#7BAFD4", "#CD660D"))
 plot = plot+ggtitle(title)
 plot = plot + theme(axis.text=element_text(size=10),axis.title=element_text(size=16,vjust=0.35),legend.text=element_text(size=12),plot.title=element_text(size=22))
 plot = plot + scale_x_continuous(name="Date", breaks=seq(2000,2100,20))
 plot = plot + scale_y_continuous(name=expression("Diseased area (m^2"))
 
-eda_mean$min_diseased_area <- eda_mean$diseased_area-(eda_sd$diseased_area/7)
-eda_mean$max_diseased_area <- eda_mean$diseased_area+(eda_sd$diseased_area/7)
-
-plot = plot +geom_ribbon(data = eda_mean, aes(ymin=min_diseased_area, ymax=max_diseased_area, fill = factor(resolution), color = factor(resolution)), alpha=0.3)
+plot = plot +geom_ribbon(data = eda_mean_quarter, aes(ymin=min_diseased_area, ymax=max_diseased_area, fill = factor(resolution), color = factor(resolution)), alpha=0.3)
 
 #plot= plot+theme(legend.position="none")
 plot
